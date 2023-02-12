db4db958b5eab37d76f84fdced66a369b4c6a9f1
Glitch <bright.glitch@gmail.com>
2023-02-12 04:08:10 -0300

    added title

--- pages/_document.tsx
+++ pages/_document.tsx
@@ -3,7 +3,10 @@ import { Html, Head, Main, NextScript } from 'next/document'
 export default function Document() {
   return (
     <Html lang="en">
-      <Head />
+      <Head>
+        <link rel="icon" href="/favicon.ico" />
+        <meta name="description" content="Metamask Zustand jasndjansdakopokp" />
+      </Head>
       <body>
         <Main />
         <div id='modal'/>
