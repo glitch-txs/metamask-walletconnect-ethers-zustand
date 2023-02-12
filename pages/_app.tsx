e06b6027e51e2d88b8d8efcb992ea6bb09721cb9
Glitch <bright.glitch@gmail.com>
2023-02-12 04:38:03 -0300

    asd

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -7,6 +7,7 @@ export default function App({ Component, pageProps }: AppProps) {
     <>
       <Head>
         <title>Metamask Zustand hehe</title>
+        <link rel="icon" href="/favicon.ico" />
         <meta property="og:title" content="Here some text title :)" />
         <meta property='og:description' content='Boilerpalte for metamask and walletconect v2'/>
         <meta property='og:url' content='https://metamask-walletconnect-ethers-zustand.vercel.app/'/>
