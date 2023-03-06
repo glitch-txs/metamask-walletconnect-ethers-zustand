f9db0b7229b57601910331ac0a4d6ca47debda90
Glitch <glitch.txs@gmail.com>
2023-03-06 08:06:08 -0300

    reusable add token function

--- actions/metamask/helpers/requestAddToken.ts
+++ actions/metamask/helpers/requestAddToken.ts
@@ -1,14 +1,21 @@
-export const addTokenRequest = (provider: any)=>{
-    provider.request({
-        method: 'wallet_watchAsset',
-        params: {
-          type: 'ERC20',
-          options: {
-            address: 'tokenAddress',
-            symbol: 'ABC',
-            decimals: 18,
-            image: 'image url of the token',
-          },
-        },
-      });
+type Props = {
+  address: string
+  symbol: string
+  decimals?: number
+  image: string
+}
+
+export const addTokenRequest = async({ address, symbol, decimals = 18, image }: Props)=>{
+  await window.ethereum.request({
+    method: 'wallet_watchAsset',
+    params: {
+      type: 'ERC20',
+      options: {
+        address: address,
+        symbol: symbol,
+        decimals: decimals,
+        image
+      },
+    },
+  });
 }
